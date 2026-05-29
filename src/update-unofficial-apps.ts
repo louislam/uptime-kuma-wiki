63081191961f6d265511f58701b17ec398f87ec0
Louis Lam <louislam@users.noreply.github.com>
2026-05-29 13:31:35 +0800

    Remove rate limit, as we have github token now.

--- src/update-unofficial-apps.ts
+++ src/update-unofficial-apps.ts
@@ -6,51 +6,47 @@ Feel free to add your project here by making a pull request in [this wiki's repo
 
 `
 
-if (new Date(obj.lastUpdate).getTime() < new Date().getTime() - 7 * 24 * 60 * 60 * 1000) {
-    console.log("Updating unofficial apps data...");
+console.log("Updating unofficial apps data...");
 
-    const githubToken = Deno.env.get("GITHUB_TOKEN");
-    const headers: HeadersInit = {};
-    if (!githubToken) {
-        throw new Error("GITHUB_TOKEN environment variable is not set.");
-    }
+const githubToken = Deno.env.get("GITHUB_TOKEN");
+const headers: HeadersInit = {};
+if (!githubToken) {
+    throw new Error("GITHUB_TOKEN environment variable is not set.");
+}
 
-    headers["Authorization"] = `token ${githubToken}`;
+headers["Authorization"] = `token ${githubToken}`;
 
-    // Update github star count
-    for (const item of obj.apps) {
-        if (item.githubRepo) {
-            console.log(`Fetching data for ${item.githubRepo}...`);
-            const url = item.githubRepo.replace("https://github.com/", "https://api.github.com/repos/");
-            try {
-                const response = await fetch(url, {
-                    headers,
-                });
-                if (response.ok) {
-                    const data = await response.json();
-                    item.githubStars = data.stargazers_count;
-                    item.lastUpdate = (new Date(data.pushed_at)).toISOString().slice(0, 10);
-        
-                } else {
-                    console.error(`Failed to fetch data for ${item.githubRepo}: ${response.statusText}`);
-                }
-            } catch (error) {
-                console.error(`Error fetching data for ${item.githubRepo}: ${error}`);
+// Update github star count
+for (const item of obj.apps) {
+    if (item.githubRepo) {
+        console.log(`Fetching data for ${item.githubRepo}...`);
+        const url = item.githubRepo.replace("https://github.com/", "https://api.github.com/repos/");
+        try {
+            const response = await fetch(url, {
+                headers,
+            });
+            if (response.ok) {
+                const data = await response.json();
+                item.githubStars = data.stargazers_count;
+                item.lastUpdate = (new Date(data.pushed_at)).toISOString().slice(0, 10);
+
+            } else {
+                console.error(`Failed to fetch data for ${item.githubRepo}: ${response.statusText}`);
             }
+        } catch (error) {
+            console.error(`Error fetching data for ${item.githubRepo}: ${error}`);
         }
     }
-
-    obj.apps.sort((a, b) => {
-        return a.name.localeCompare(b.name);
-    });
-
-    obj.lastUpdate = new Date().toISOString();
-
-    // Store JSON
-    Deno.writeTextFileSync("./src/unofficial-apps.json", JSON.stringify(obj, null, 4));
-} else {
-    console.log("Don't need to update unofficial apps data, last update was less than 7 days ago.");
 }
+
+obj.apps.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+});
+
+obj.lastUpdate = new Date().toISOString();
+
+// Store JSON
+Deno.writeTextFileSync("./src/unofficial-apps.json", JSON.stringify(obj, null, 4));
 
 // Sort by github stars, and then by name
 obj.apps.sort((a, b) => {
