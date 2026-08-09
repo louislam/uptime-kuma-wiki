ef52f091f29738a7f7decf4432dd1045cc4492a8
Louis Lam <louislam@users.noreply.github.com>
2026-08-10 04:03:30 +0800

    Add star history svg (#173)
    * Add star history svg
    
    * Add star history license

---
+++ src/star-history/chart-types.ts
@@ -0,0 +1,17 @@
+export type ChartMode = "Date" | "Timeline";
+
+export type LegendPosition = "top-left" | "bottom-right";
+
+export interface StarRecord {
+    date: string;
+    count: number;
+}
+
+export interface RepoStarData {
+    repo: string;
+    starRecords: StarRecord[];
+}
+
+export interface RepoData extends RepoStarData {
+    logoUrl: string;
+}
